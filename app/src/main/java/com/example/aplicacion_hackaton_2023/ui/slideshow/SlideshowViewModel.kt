5459c1c9a95d89b9ca0dba78258f8c1168923ada
tinglypuma4 <esauboycj@gmail.com>
2023-04-21 11:34:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/aplicacion_hackaton_2023/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.aplicacion_hackaton_2023.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
